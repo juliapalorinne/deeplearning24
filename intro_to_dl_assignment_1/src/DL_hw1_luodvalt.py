8b2f98c709761031cc8292c7bbf8893903b2890f
juliapalorinne <julia.palorinne@helsinki.fi>
2024-02-02 23:30:06 +0200

    assingment 1

--- intro_to_dl_assignment_1/src/DL_hw1_luodvalt.py
+++ intro_to_dl_assignment_1/src/DL_hw1_luodvalt.py
@@ -1,8 +1,8 @@
 """
    Introduction to Deep Learning (LDA-T3114)
-   Skeleton Code for Assignment 1: Sentiment Classification on a Feed-Forward Neural Network
+   Assignment 1: Sentiment Classification on a Feed-Forward Neural Network
 
-   Hande Celikkanat & Miikka Silfverberg
+   Valtteri Luodemäki & Julia Palorinne
 """
 
 import numpy as np
@@ -52,23 +52,29 @@ def label_to_idx(label):
     return torch.LongTensor([LABEL_INDICES[label]])
 
 
-
 #--- model ---
 
 class FFNN(nn.Module):
     # Feel free to add whichever arguments you like here.
-    def __init__(self, vocab_size, n_classes, extra_arg_1=None, extra_arg_2=None):
+    def __init__(self, vocab_size, n_classes, hidden1_n, hidden2_n):
         super(FFNN, self).__init__()
-        # WRITE CODE HERE
-        pass
+        self.hidden1 = nn.Linear(vocab_size, hidden1_n)
+        self.relu1 = nn.ReLU()
+        # self.hidden2 = nn.Linear(hidden1_n, hidden2_n)
+        self.output = nn.Linear(hidden1_n, n_classes)
 
     def forward(self, x):
-        # WRITE CODE HERE
-        pass
+        x = self.hidden1(x)
+        x = self.relu1(x)
+        # x = self.hidden2(x)
+        x = self.output(x)
+        x = torch.nn.functional.log_softmax(x, dim=1)
+        return x
 
 
 
 #--- data loading ---
+
 data = read_semeval_datasets(data_dir)
 indices, vocab_size = generate_bow_representations(data)
 
@@ -76,45 +82,66 @@ indices, vocab_size = generate_bow_representations(data)
 
 #--- set up ---
 
-# WRITE CODE HERE
-model = FFNN(vocab_size, N_CLASSES) #add extra arguments here if you use
-loss_function = None
-optimizer = None
+hidden1_size = 12
+hidden2_size = 3
+model = FFNN(vocab_size, N_CLASSES, hidden1_size, hidden2_size) #add extra arguments here if you use
+loss_function = torch.nn.NLLLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
 
 
 
 #--- training ---
+
 for epoch in range(N_EPOCHS):
     total_loss = 0
     # Generally speaking, it's a good idea to shuffle your
     # datasets once every epoch.
-    random.shuffle(data['training'])    
+    random.shuffle(data['training'])
 
     for i in range(int(len(data['training'])/BATCH_SIZE)):
         minibatch = data['training'][i*BATCH_SIZE:(i+1)*BATCH_SIZE]
+        optimizer.zero_grad()
 
-        # WRITE CODE HERE            
-        pass
-                              
+        for tweet in minibatch:
+            target = label_to_idx(tweet['SENTIMENT'])
+            input = model.forward(tweet['BOW'])
+            train_loss = loss_function(input, target)
+            train_loss.backward()
+            optimizer.step()
+            total_loss += train_loss
+        
+        # inputs = []
+        # targets = []
+        # for tweet in minibatch:
+        #     targets.append(label_to_idx(tweet['SENTIMENT']))
+        #     inputs.append(tweet['BOW'])        
+
+        # input_tensor = torch.stack(inputs)
+        # target_tensor = torch.stack(targets)
+        # output = model.forward(input_tensor)
+        # train_loss = loss_function(output, target_tensor)
+        # train_loss.backward()
+        # optimizer.step()
+        # total_loss += train_loss
+        
     if ((epoch+1) % REPORT_EVERY) == 0:
         print('epoch: %d, loss: %.4f' % (epoch+1, total_loss*BATCH_SIZE/len(data['training'])))
 
 
-
 #--- test ---
+
 correct = 0
 with torch.no_grad():
     for tweet in data['test.gold']:
         gold_class = label_to_idx(tweet['SENTIMENT'])
 
-        # WRITE CODE HERE
-        # You can, but for the sake of this homework do not have to,
-        # use batching for the test data.
-        predicted = -1
+        predicted = torch.argmax(torch.exp(model.forward(tweet['BOW'])))
+        if gold_class.item() == predicted.item():
+            correct += 1
+        
 
         if IS_VERBOSE:
-            print('TEST DATA: %s, GOLD LABEL: %s, GOLD CLASS %d, OUTPUT: %d' % 
-                 (' '.join(tweet['BODY'][:-1]), tweet['SENTIMENT'], gold_class, predicted))
+           print('TEST DATA: %s, GOLD LABEL: %s, GOLD CLASS %d, OUTPUT: %d' % 
+                (' '.join(tweet['BODY'][:-1]), tweet['SENTIMENT'], gold_class, predicted))
 
     print('test accuracy: %.2f' % (100.0 * correct / len(data['test.gold'])))
-
