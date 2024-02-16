2113980c0a136f6b47ed578e185a9038580c3028
juliapalorinne <julia.palorinne@helsinki.fi>
2024-02-16 17:13:19 +0200

    regularization testing

--- intro_to_dl_assignment_2/src/DL_hw2_luodvalt.py
+++ intro_to_dl_assignment_2/src/DL_hw2_luodvalt.py
@@ -7,15 +7,14 @@ from torchvision import transforms, datasets
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-import random
 import os
 os.chdir('C:/Users/julia/OneDrive/Tiedostot/Opiskelu/DeepLearning/intro_to_dl_assignment_2/')
 
 #--- hyperparameters ---
-N_EPOCHS = 8
+N_EPOCHS = 20
 BATCH_SIZE_TRAIN = 100
 BATCH_SIZE_TEST = 100
-LR = 0.02
+LR = 0.01
 #--- fixed constants ---
 NUM_CLASSES = 24
 DATA_DIR = './data/sign_mnist_%s'
@@ -50,6 +49,7 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=5, padding=2) # 28*28 -> 14*14
         self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.2)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=5, padding=2) # 14*14 -> 7*7
         # self.fc1 = nn.Linear(64*14*14, 196)
         self.fc1 = nn.Linear(128*7*7, 196)
@@ -58,6 +58,7 @@ class CNN(nn.Module):
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
         x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
@@ -80,23 +81,29 @@ loss_function  = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
 # optimizer = optim.SGD(model.parameters(), lr=0.05)                                # test accuracy 89.3%, 10 epochs, no regularization
 # optimizer = optim.SGD(model.parameters(), lr=0.01)                                # test accuracy 59%, 8 epochs, no regularization
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.999))     # test accuracy 11.4%, 8 epochs, no regularization
 # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)                  # test accuracy 65%, 8 epochs, no regularization
 # optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.9)                  # test accuracy 86.2%, 8 epochs, no regularization
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, betas=(0.9, 0.999))     # test accuracy 11.4%, 8 epochs, no regularization
+# optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9)                 # test accuracy 84.2%, 20 epochs, early stopping regularization (stopped after 6 epochs)
+# optimizer = optim.SGD(model.parameters(), lr=0.002, momentum=0.9)                 # test accuracy 77.6%, 20 epochs, early stopping regularization (stopped after 13 epochs)
+# optimizer = optim.SGD(model.parameters(), lr=0.002, momentum=0.9)                 # test accuracy 85.2%, 20 epochs, dropout and early stopping regularization (stopped after 19 epochs)
+# optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)                  # test accuracy %, 20 epochs, dropout and early stopping regularization (stopped after  epochs)
+# optimizer = optim.SGD(model.parameters(), lr=0.01)                                # test accuracy %, 20 epochs, early stopping regularization (stopped after  epochs)
+# optimizer = optim.SGD(model.parameters(), lr=0.01)                                # test accuracy %, 20 epochs, dropout regularization
 optimizer_adagrad = torch.optim.Adagrad(model.parameters(), lr=LR)
 
 
 #--- training ---
+last_error = list()
 for epoch in range(N_EPOCHS):
     train_loss = 0
     train_correct = 0
     total = 0
-    random.shuffle(train_loader)
 
     for batch_num, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
-        outputs = model.forward(data)
+        outputs = model(data)
         
         loss = loss_function(outputs, target)
         loss.backward()
@@ -112,7 +119,7 @@ for epoch in range(N_EPOCHS):
 
 
         print('Training: Epoch %d - Batch %d/%d: Loss: %.4f | Train Acc: %.3f%% (%d/%d)' % 
-              (epoch, batch_num, len(train_loader), train_loss / (batch_num + 1), 
+              (epoch + 1, batch_num + 1, len(train_loader), train_loss / (batch_num + 1), 
                100. * train_correct / total, train_correct, total))
     
     dev_loss = 0
@@ -122,7 +129,7 @@ for epoch in range(N_EPOCHS):
         for batch_num, (data, target) in enumerate(dev_loader):
             data, target = data.to(device), target.to(device)
 
-            outputs = model.forward(data)
+            outputs = model(data)
             loss = loss_function(outputs, target)
 
             predicted = torch.zeros(target.size(dim=0))
@@ -132,11 +139,16 @@ for epoch in range(N_EPOCHS):
             dev_loss += loss.item()
             dev_correct += (target == predicted).sum()
             dev_total += BATCH_SIZE_TEST
+            dev_error = 1 - dev_correct / dev_total
 
-            print('Evaluating: Batch %d/%d: Loss: %.4f | Dev Acc: %.3f%% (%d/%d)' % 
-                (batch_num, len(test_loader), dev_loss / (batch_num + 1), 
-                100. * dev_correct / total, dev_correct, dev_total))
-
+            print('Validating: Batch %d/%d: Loss: %.4f | Dev Acc: %.3f%% (%d/%d)' % 
+                (batch_num + 1, len(dev_loader), dev_loss / (batch_num + 1), 
+                100. * dev_correct / dev_total, dev_correct, dev_total))
+        
+        if epoch > 1 and last_error[epoch-1] < dev_error and last_error[epoch-2] < dev_error:
+            break
+        else:
+            last_error.append(dev_error)
 
 
 #--- test ---
@@ -145,7 +157,6 @@ test_correct = 0
 total = 0
 
 with torch.no_grad():
-    random.shuffle(test_loader)
     for batch_num, (data, target) in enumerate(test_loader):
         data, target = data.to(device), target.to(device)
 
@@ -161,6 +172,6 @@ with torch.no_grad():
         total += BATCH_SIZE_TEST
 
         print('Evaluating: Batch %d/%d: Loss: %.4f | Test Acc: %.3f%% (%d/%d)' % 
-              (batch_num, len(test_loader), test_loss / (batch_num + 1), 
+              (batch_num + 1, len(test_loader), test_loss / (batch_num + 1), 
                100. * test_correct / total, test_correct, total))
 
